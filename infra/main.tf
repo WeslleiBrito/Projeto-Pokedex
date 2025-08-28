7f30f6798d3d464a3bf2f3bb83cfef8989e633d4
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2025-08-28 00:47:42 +0000

    correcao infra

--- infra/main.tf
+++ infra/main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 # --- ECR repo
 resource "aws_ecr_repository" "repo" {
   name = var.ecr_repo_name
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
