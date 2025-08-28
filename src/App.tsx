f288d6a411031e91b4a2dc7adf2c2853f5917877
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2025-08-28 18:01:33 +0000

    modificado o app para testar se atualiza o site

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,6 @@ export default function App() {
             alt="Pokédex background"
             className="pokedex-bg"
           />
-          <h1>Teste de mudança no código e não na infra</h1>
           <div className="pokemon-center">
             <img
               src={sprite}
